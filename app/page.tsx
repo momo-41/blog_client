0c1185ea7eee1cb009fde5f104f759dcf49078d2
momo-41 <minimizu41@gmail.com>
2024-10-29 19:22:34 +0900

    型定義だけ

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Post } from "./const/types";
 
-const Page = () => {
-  return <div>Page</div>;
+type Props = {
+  posts: Post[];
 };
 
-export default Page;
+async function fetchPosts() {
+  const res = await fetch("http://localhost:3001/api/v1/posts");
+  const posts = await res.json();
+  return posts;
+}
+
+export default async function Page() {
+  const posts = await fetchPosts();
+  console.log(posts); // ターミナルに出力されます
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <pre>{JSON.stringify(posts, null, 2)}</pre>
+    </div>
+  );
+}
